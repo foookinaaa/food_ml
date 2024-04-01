322e39817e4f518d9b0d180dc0c19fa97b4e04fd
Iuliia Fokina <iuliia.fokina@kindredgroup.com>
2024-04-01 22:22:54 +0100

    init

---
+++ tests/app/test_example.py
@@ -0,0 +1,9 @@
+from food_ml.app.example import is_positive
+
+
+def test_is_positive_true():
+    assert is_positive(5) is True
+
+
+def test_is_positive_false():
+    assert is_positive(-5) is False
