7752ff40ab74144805fe8ca04e6fb6f83baa31e9
Iuliia Fokina <iuliia.fokina@kindredgroup.com>
2024-04-25 10:16:09 +0100

    add hydra config

--- src/mlops_ods/train_predict.py
+++ src/mlops_ods/train_predict.py
@@ -1,52 +1,53 @@
 import os
 
 import catboost
+import hydra
 import pandas as pd
+from omegaconf import DictConfig
 from sklearn import metrics
 
 import mlops_ods.utils.utils_etl as ut_etl
 import mlops_ods.utils.utils_model as ut_models
-from mlops_ods.config import load_config_yaml
 from mlops_ods.utils import utils_etl
 
-PATH_TO_DATA = os.path.join(os.path.dirname(os.getcwd()), "mlops_ods", "dataset")
-PATH_TO_MODEL = os.path.join(os.path.dirname(os.getcwd()), "resources", "model.bin")
 logger = utils_etl.get_logger(__name__)
 logger.propagate = False
 
 
-def main(train: bool = True):
-    """
-    Train or predict data with model
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig):
+    path_to_data = os.path.join(os.path.dirname(os.getcwd()), cfg.settings.path_to_data)
+    path_to_model = os.path.join(
+        os.path.dirname(os.getcwd()),
+        cfg.settings.path_to_model,
+        cfg.settings.model_name,
+    )
+    train_mode = cfg.settings.train_mode
 
-    :param train: if true - train, if false - predict
-    :return:
-    """
-    mode = "train" if train else "predict"
+    mode = "train" if train_mode else "predict"
     logger.info("Start job in mode %s", mode)
-    config = load_config_yaml()
 
     logger.info("Get data")
-    file_name = "2015-street-tree-census-tree-data.csv"
-    ut_etl.download_kaggle_dataset_if_not_exist(PATH_TO_DATA, file_name)
+    file_name = cfg.settings.file_name
+    ut_etl.download_kaggle_dataset_if_not_exist(path_to_data, file_name)
 
-    df = pd.read_csv(f"{PATH_TO_DATA}/{file_name}")
+    df = pd.read_csv(f"{path_to_data}/{file_name}")
     df = df[~df["health"].isna()]
 
     logger.info("Preprocess data")
     ut_models.drop_columns(df)
     ut_models.preprocess_data(df)
 
-    num_cols = config["features"]["numerical"]
-    cat_cols = config["features"]["categorical"]
+    num_cols = cfg.features.numerical
+    cat_cols = cfg.features.categorical
     total_cols = num_cols + cat_cols
 
-    if train:
+    if train_mode:
         logger.info("Fit model")
         clf = catboost.CatBoostClassifier(
-            iterations=config["model_params"]["iterations"],
-            verbose=config["model_params"]["verbose"],
-            random_seed=config["model_params"]["random_seed"],
+            iterations=cfg.model.iterations,
+            verbose=cfg.model.verbose,
+            random_seed=cfg.model.random_seed,
             cat_features=cat_cols,
         )
         clf.fit(df[total_cols], df["health"])
@@ -55,14 +56,14 @@ def main(train: bool = True):
         logger.info("Roc auc score: %s", roc_auc)
 
         logger.info("Save model")
-        ut_etl.save_model(model_path=PATH_TO_MODEL, model=clf)
+        ut_etl.save_model(model_path=path_to_model, model=clf)
 
     else:
         logger.info("Predict")
-        clf = ut_etl.load_model(PATH_TO_MODEL)
+        clf = ut_etl.load_model(path_to_model)
         predictions = clf.predict_proba(df[total_cols])
         logger.info("Number of predictions: %s", len(predictions))
 
 
 if __name__ == "__main__":
-    main(train=True)
+    main()
