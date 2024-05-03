b582eb3b01b63266d47e365559da62877401e1b0
Iuliia Fokina <iuliia.fokina@kindredgroup.com>
2024-05-03 15:57:27 +0100

    fix scaler dvc

--- src/mlops_ods/dvc_functions/scaler.py
+++ src/mlops_ods/dvc_functions/scaler.py
@@ -45,18 +45,20 @@ def cli_train_scaler(
     df = pd.read_csv(input_frame_path)
     scaler, train, test = train_scaler(df[num_cols])
     pickle.dump(scaler, scaler_path.open("wb"))
-    train.to_csv(train_features_path, index=False)
-    test.to_csv(test_features_path, index=False)
+    train.join(df["health"]).to_csv(train_features_path, index=False)
+    test.join(df["health"]).to_csv(test_features_path, index=False)
 
 
 @cli.command()
 @click.argument("input_frame_path", type=Path)
 @click.argument("scaler_path", type=Path)
 @click.argument("result_frame_path", type=Path)
+@click.argument("target_frame_path", type=Path)
 def cli_apply_scaler(
     input_frame_path: Path,
     scaler_path: Path,
     result_frame_path: Path,
+    target_frame_path: Path,
 ):
     cfg = compose_config()
     num_cols = cfg.features.numerical
@@ -65,3 +67,4 @@ def cli_apply_scaler(
     scaler = pickle.load(scaler_path.open("rb"))
     result = apply_scaler(scaler, df[num_cols])
     np.save(result_frame_path, result)
+    np.save(target_frame_path, df["health"])
