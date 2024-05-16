fc95a257cbbd69cb64af379b06c190f50027b06c
Iuliia Fokina <iuliia.fokina@kindredgroup.com>
2024-05-16 13:30:28 +0100

    add clearml pipelines

--- src/clear_ml/pipeline_model.py
+++ src/clear_ml/pipeline_model.py
@@ -1,37 +1,6 @@
 from clearml import PipelineController
 
-
-def step_one():
-    import pandas as pd  # noqa
-    from clearml import Dataset
-
-    frame_path = Dataset.get(
-        dataset_name="Raw data", dataset_project="Mlops-test"
-    ).get_local_copy()
-    data = pd.read_csv(frame_path + "/2015-street-tree-census-tree-data.csv")
-    return data
-
-
-def step_two(df, test_size=0.3, random_state=42):
-    import pandas as pd  # noqa
-    from sklearn.model_selection import train_test_split
-
-    from mlops_ods.config import compose_config
-    from mlops_ods.utils.utils_model import drop_columns, preprocess_data
-
-    cfg = compose_config()
-    num_cols = cfg.features.numerical
-    cat_cols = cfg.features.categorical
-    df = df[~df["health"].isna()]
-    drop_columns(df)
-    preprocess_data(df)
-    y = df["health"]
-    X = df[num_cols + cat_cols]
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_size, random_state=random_state
-    )
-
-    return X_train, X_test, y_train, y_test
+from clear_ml.pipeline_data_etl import step_one, step_two
 
 
 def step_three(data):
@@ -44,7 +13,7 @@ def step_three(data):
     cfg = compose_config()
     cat_cols = cfg.features.categorical
     model_params = {
-        "iterations": cfg.model.iterations,
+        "iterations": 800,
         "verbose": cfg.model.verbose,
         "random_seed": cfg.model.random_seed,
         "cat_features": cat_cols,
@@ -64,8 +33,6 @@ def step_four(data, model):
 
 
 if __name__ == "__main__":
-
-    # create the pipeline controller
     pipe = PipelineController(
         project="Mlops-test",
         name="Pipeline catboost long",
@@ -87,6 +54,7 @@ if __name__ == "__main__":
         function_return=["processed_data"],
         cache_executed_step=True,
     )
+
     pipe.add_function_step(
         name="step_three",
         # parents=['step_two'],
